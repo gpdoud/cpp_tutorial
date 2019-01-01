7f7d222d02cba12674a7efb3a81bafba4a44dde7
Greg Doud <gpdoud@gmail.com>
2019-01-01 13:45:35 -0500

    added Chapter_04 linked list

---
+++ Beginning_C++/Chapter_04/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+struct task {
+    task *next;
+    std::string description;
+};
+
+void add(const std::string description, const std::string afterDescription);
+void append(const std::string description);
+void cleanup();
+task * find(const std::string description);
+
+
+task *HEAD = nullptr, *TAIL = nullptr;
+
+task * find(const std::string description) {
+    task *t = HEAD;
+    while(t != nullptr) {
+        if(t->description == description) {
+            return t;
+        }
+    }
+    return nullptr;
+}
+
+void add(const std::string description, const std::string afterDescription) {
+    task *aftask = find(afterDescription);
+    if(aftask == nullptr) {
+        append(description);
+        return;
+    }
+    // if not appended, must fix it in
+    task *t = new task;
+    t->description = description;
+    t->next = aftask->next;
+    aftask->next = t;
+
+}
+
+void append(const std::string description) {
+    // construct the task
+    task *t = new task;
+    t->next = nullptr;
+    t->description = description;
+
+    // if list is empty, place this at the HEAD
+    if(HEAD == nullptr) {
+        HEAD = TAIL = t;
+        return;
+    }
+    // add to the end
+    TAIL->next = t;
+    TAIL = t;
+}
+
+void cleanup() {
+
+    // clean-up
+    task *t = HEAD;
+    while(t != nullptr) {
+        std::cout << t->description << std::endl;
+        task *next = t->next;
+        delete t;
+        t = next;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    append("Genesis task");
+    append("1st task");
+    append("2nd task");
+    // add("0th task", "1st task");
+    cleanup();
+    std::cout << "Done..." << std::endl;
+    return 0;
+}
