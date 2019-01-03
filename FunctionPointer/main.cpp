3ed91ed0225deebbd24158db08b66dcb97cf37d1
Greg Doud <gpdoud@gmail.com>
2019-01-03 07:53:29 -0500

    "change FunctionPointer to use using syntax"

--- FunctionPointer/main.cpp
+++ FunctionPointer/main.cpp
@@ -8,7 +8,8 @@ int sub(const int p1, const int p2) {
 }
 int main(int argc, char *argv[]) {
 
-    int (*fn)(int, int); // function pointer
+    using func = int (*)(int, int); // function pointer
+    func fn;
     int p1 = 5, p2 = 3;
     fn = add;
     std::cout << "sum is " << fn(p1, p2);
